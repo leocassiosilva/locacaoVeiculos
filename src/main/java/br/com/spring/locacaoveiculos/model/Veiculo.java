60b779319fc392dfdad95ed0b34967786ac78cb8
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-13 11:54:12 -0300

    mais+

--- src/main/java/br/com/spring/locacaoveiculos/model/Veiculo.java
+++ src/main/java/br/com/spring/locacaoveiculos/model/Veiculo.java
@@ -31,57 +31,81 @@ public class Veiculo extends AbstractEntity<Long> {
 	private List<Arquivo> arquivos;
 	
 	
-	@ManyToOne
+	@ManyToOne(optional = true)
 	@JoinColumn(name = "id_categoria")
 	private Categoria categoria;
-	
+
+
 	public String getNome() {
 		return nome;
 	}
+
 
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
 
+
 	public Marca getMarca() {
 		return marca;
 	}
+
 
 	public void setMarca(Marca marca) {
 		this.marca = marca;
 	}
 
+
 	public Tipo getTipo() {
 		return tipo;
 	}
+
 
 	public void setTipo(Tipo tipo) {
 		this.tipo = tipo;
 	}
 
+
 	public String getPlaca() {
 		return placa;
 	}
+
 
 	public void setPlaca(String placa) {
 		this.placa = placa;
 	}
 
+
 	public String getRenavan() {
 		return renavan;
 	}
+
 
 	public void setRenavan(String renavan) {
 		this.renavan = renavan;
 	}
 
+
 	public List<Arquivo> getArquivos() {
 		return arquivos;
 	}
 
+
 	public void setArquivos(List<Arquivo> arquivos) {
 		this.arquivos = arquivos;
 	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
 	
 	
 
