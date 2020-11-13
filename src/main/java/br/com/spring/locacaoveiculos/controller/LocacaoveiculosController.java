75ec1125c017c0ad36af9bcc520e96bd9c3445a4
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-13 07:49:05 -0300

    \home

--- src/main/java/br/com/spring/locacaoveiculos/controller/LocacaoveiculosController.java
+++ src/main/java/br/com/spring/locacaoveiculos/controller/LocacaoveiculosController.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LocacaoveiculosController {
 
-	
-
 	/* Para chamar o metodo da pagina inicial */
 	@RequestMapping(value = "/home", method = RequestMethod.GET)
 	public String home() {
-		return "usuario/home";
+		return "/home";
 	}	
 	
 }
