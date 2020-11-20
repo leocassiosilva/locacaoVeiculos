f70e52f8198522e77e817b6b901c04fc8b13de9b
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-20 18:53:16 -0300

    classe local

--- src/main/java/br/com/spring/locacaoveiculos/model/local.java
+++ src/main/java/br/com/spring/locacaoveiculos/model/local.java
@@ -1,5 +1,21 @@
 package br.com.spring.locacaoveiculos.model;
 
-public class local {
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "LOCAL")
+public class Local extends AbstractEntity<Long>{
+	
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
 }
