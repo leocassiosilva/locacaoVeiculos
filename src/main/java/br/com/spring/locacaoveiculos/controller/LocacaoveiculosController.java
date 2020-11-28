b84883da7502f41683b695de95b5b390235fafd7
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-28 14:08:00 -0300

    arrumando tela de informações de veiculo

--- src/main/java/br/com/spring/locacaoveiculos/controller/LocacaoveiculosController.java
+++ src/main/java/br/com/spring/locacaoveiculos/controller/LocacaoveiculosController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.spring.locacaoveiculos.model.Seguro;
@@ -40,6 +41,14 @@ public class LocacaoveiculosController {
 	@ModelAttribute("seguros")
 	public List<Seguro> listaDeDepartamentos() {
 		return seguroService.buscarTodos();
+	}
+	
+	
+	@PostMapping("/pagar")
+	public String pagarLocacao() {
+		System.out.println("Pague a locação rapaz!");
+		return "/locacao/pagarLocacao";
+
 	}
 	
 }
