44f9a00c828b820d96a3321b13f808a90f626f39
leocassiosilva <leocassiosilva1234@gmail.com>
2020-11-29 17:46:14 -0300

    service local

---
+++ src/main/java/br/com/spring/locacaoveiculos/service/LocalService.java
@@ -0,0 +1,10 @@
+package br.com.spring.locacaoveiculos.service;
+
+import java.util.List;
+
+import br.com.spring.locacaoveiculos.model.Local;
+
+
+public interface LocalService {
+	List<Local> buscarTodos();
+}
