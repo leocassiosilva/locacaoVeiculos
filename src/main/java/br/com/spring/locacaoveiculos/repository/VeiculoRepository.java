13ce42095a75e878cbdfa52e34114b9c437f6d5a
Felipe Gabriel <fgabrielqr@gmail.com>
2020-12-14 09:53:01 -0300

    Erro

--- src/main/java/br/com/spring/locacaoveiculos/repository/VeiculoRepository.java
+++ src/main/java/br/com/spring/locacaoveiculos/repository/VeiculoRepository.java
@@ -7,32 +7,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
 import br.com.spring.locacaoveiculos.model.Veiculo;
 
-public interface VeiculoRepository extends JpaRepository<Veiculo, Long>{
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
 
-	
-	@Query(name="buscarTodos", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)", nativeQuery = true)
+	@Query(name = "buscarTodos", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)", nativeQuery = true)
 	List<Veiculo> buscarTodos();
-	
-	@Query(name="buscarPeloId", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
+
+	@Query(name = "buscarPeloId", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
 			+ "where v.id = :id", nativeQuery = true)
 	Optional<Veiculo> buscarPeloId(@Param("id") Long id);
-	
-	@Query(name=" buscarVeiculo", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
+
+	@Query(name = " buscarVeiculo", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
 			+ "inner join tipomarca on(v.id_tipo_marca = tipomarca.id)\n"
 			+ "inner join marca on(tipomarca.id_marca = marca.id)\n"
-			+ "inner join locadora on(v.id_locadora = locadora.id)\n"
-			+ "where v.id = :id", nativeQuery = true)
+			+ "inner join locadora on(v.id_locadora = locadora.id)\n" + "where v.id = :id", nativeQuery = true)
 	Veiculo buscarVeiculo(@Param("id") Long id);
-<<<<<<< HEAD
-	
-			
-=======
-		
-	@Query(name="buscarPorNome", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
+
+	@Query(name = "buscarPorNome", value = "Select * FROM veiculos as v inner join arquivos as ar on(v.id = ar.id_veiculo)\n"
 			+ "where v.id = :id", nativeQuery = true)
-	List<Veiculo> buscarPorNome(@Param("id")Long id);
->>>>>>> 6d959668fd3107a489e2800381cb134d58b2aa78
+	List<Veiculo> buscarPorNome(@Param("id") Long id);
 }
