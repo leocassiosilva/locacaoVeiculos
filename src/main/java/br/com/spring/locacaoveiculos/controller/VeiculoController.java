10090c6fcf7034e95a6b4e889e66dbbe819d4b3f
leocassiosilva <leocassiosilva1234@gmail.com>
2020-12-25 08:00:08 -0300

    algumas correções

--- src/main/java/br/com/spring/locacaoveiculos/controller/VeiculoController.java
+++ src/main/java/br/com/spring/locacaoveiculos/controller/VeiculoController.java
@@ -159,13 +159,7 @@ public class VeiculoController {
 		return tipoService.buscarTodosTipos();
 	}
 
-	@GetMapping("/listar")
-	public String listar(ModelMap model) {
-		System.out.println("Pronto");
-		List<Veiculo> veiculo = veiculoService.buscarTodos();
-		model.addAttribute("veiculos", veiculo);
-		return "veiculo/lista";
-	}
+	
 	
 	@GetMapping("/buscar")
 	public String getPorDatas(@RequestParam("dataRetirar") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataRetirar,@RequestParam("dataDevolver") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dataDevolver,  @RequestParam("nome") String nome,  ModelMap model) {
