1c70ea03197556a2eb74b4bb1e55f784bd8151aa
leocassiosilva <leocassiosilva1234@gmail.com>
2020-12-26 20:23:37 -0300

    teste commit

--- src/main/java/br/com/spring/locacaoveiculos/LocacaoVeiculosApplication.java
+++ src/main/java/br/com/spring/locacaoveiculos/LocacaoVeiculosApplication.java
@@ -3,6 +3,7 @@ package br.com.spring.locacaoveiculos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LocacaoVeiculosApplication {
 
