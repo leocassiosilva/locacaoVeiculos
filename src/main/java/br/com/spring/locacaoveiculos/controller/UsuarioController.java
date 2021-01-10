bf7be507248461b500be3076b2505ad101496f66
Felipe Gabriel <fgabrielqr@gmail.com>
2021-01-10 20:52:42 -0300

    Sessions

--- src/main/java/br/com/spring/locacaoveiculos/controller/UsuarioController.java
+++ src/main/java/br/com/spring/locacaoveiculos/controller/UsuarioController.java
@@ -1,6 +1,5 @@
 package br.com.spring.locacaoveiculos.controller;
 
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.spring.locacaoveiculos.model.Usuario;
 import br.com.spring.locacaoveiculos.service.UsuarioService;
@@ -18,13 +18,11 @@ public class UsuarioController {
 	@Autowired
 	UsuarioService usuarioService;
 
-	
 	@RequestMapping(value = "/newUsuario", method = RequestMethod.GET)
 	public String form(Usuario usuario) {
 		return "usuario/cadastroUsuario";
 	}
 
-	
 	@RequestMapping(value = "/salvar", method = RequestMethod.POST)
 	public String cadastrando(Usuario usuario) {
 		usuarioService.save(usuario);
@@ -37,17 +35,25 @@ public class UsuarioController {
 	}
 
 	@PostMapping("/logar")
-	public String logar(Usuario usuario) {
+	public ModelAndView logar(Usuario usuario, HttpSession session) {
 		Usuario userLogado = usuarioService.login(usuario);
-		return "redirect:/painel";
+		if (userLogado == null) {
+			return new ModelAndView("redirect:logar").addObject("fail", "E-mail e/ou senha incorreto");
+		}
+
+		session.setAttribute("iduser", userLogado.getId());
+		session.setAttribute("userlogado", userLogado.getEmail());
+		ModelAndView mv = new ModelAndView("redirect:/painel").addObject("logado", session.getAttribute("userlogado"));
+
+		return mv;
 	}
-	
+
 	@RequestMapping("logout")
 	public String logout(HttpSession session) {
 		session.invalidate();
 		return "redirect:/login";
 	}
-	
+
 	@RequestMapping(value = "/painel", method = RequestMethod.GET)
 	public String painel(Usuario usuario) {
 		return "usuario/painel";
